95b53fc0e0188dcac18d087ac7af103ff2c16611
Rohith0750 <rohithsd0222@gmail.com>
2025-07-28 19:38:42 +0530

    Added

---
+++ httpserver/index.js
@@ -0,0 +1,11 @@
+//const express = require('express');
+
+const express = require('express')
+const app = express()//create an instance of express
+const port = 3000
+
+app.get('/', function(req, res) {
+  res.send('Hello World! This is my first Express server.')
+})
+
+app.listen(port)
