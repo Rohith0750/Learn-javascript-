b307c40a1dd6f8335b90e7b67f2a59f3b43c18c1
Rohith0750 <rohithsd0222@gmail.com>
2025-08-08 12:40:44 +0530

    1sr sever has ran

---
+++ httpserver/index_1.js
@@ -0,0 +1,20 @@
+//This is to create a http server based on ur logic ..  demo how to run the server #node index_1.js
+const express= require("express");
+
+function Calculate_sum(n){
+    let ans = 0;
+    for(let i =0;i<n;i++){
+        ans =ans+i;
+
+    }
+    return ans;
+}
+
+const app = express();
+app.get('/', function(req,res){
+    const n = req.query.n;
+    const ans= Calculate_sum(n)
+    res.send(ans.toString());
+})
+
+app.listen(3000);
